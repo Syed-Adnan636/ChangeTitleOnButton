626c7fe87a3e537675129ec738e7c610bc164fea
Syed Adnan <syedadnansafdar6369@gmail.com>
2023-12-12 22:11:56 +0500

    Title Changing

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ChangeTitle from './ChangeTitle/changeTitle'
+
+const App = () => {
+  return (
+    <>
+      <ChangeTitle/>
+    </>
+  )
+}
+
+export default App
